67d8c7e995a6c87f9ba9ae8f9e330317cc13064c
berkay <berkayoruc@hotmail.com>
2021-03-06 11:47:26 +0300

    cluster

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
-var geojsonMarkerOptions = {
-  radius: 8,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8,
-};
-
 $(document).ready(function () {
-  var mymap = L.map("map").setView([51.505, -0.09], 3);
+  var mymap = L.map("map", { zoomControl: false }).setView([51.505, -0.09], 3);
+  L.control
+    .zoom({
+      position: "bottomright",
+    })
+    .addTo(mymap);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '<a href="https://www.itu.edu.tr/">ITU</a> | <a href="https://earthquake.usgs.gov/">USGS</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mymap);
-  getEqs("all", "day", mymap);
+  var markers = L.markerClusterGroup({
+    showCoverageOnHover: false,
+    iconCreateFunction: function (cluster) {
+      return L.divIcon({
+        html: cluster.getChildCount(),
+        className: "mycluster",
+        iconSize: L.point(50, 50),
+      });
+    },
+  });
+  getEqs("all", "month", mymap, markers);
 });
 
-function getEqs(mag, time, map) {
+function getEqs(mag, time, map, markers) {
   $.ajax({
     url: `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${time}.geojson`,
     success: function (data) {
+      markers.clearLayers();
       if (data && data.features.length > 0) {
-        L.geoJSON(data, {
+        var geojsonLayer = L.geoJSON(data, {
           pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+            var color =
+              feature.properties.mag < 1
+                ? "#78c4d4"
+                : feature.properties.mag < 2
+                ? "#61b15a"
+                : feature.properties.mag < 3
+                ? "#f88f01"
+                : feature.properties.mag < 4
+                ? "#fa1e0e"
+                : "#8c0000";
+            return L.circleMarker(latlng, {
+              radius: 12,
+              fillColor: color,
+              color: color,
+              weight: feature.properties.mag * 5,
+              opacity: 0.6,
+              fillOpacity: 1,
+            });
           },
-        })
-          .bindPopup(function (layer) {
-            return layer.feature.properties.mag.toString();
-          })
-          .addTo(map);
+        }).bindPopup(function (layer) {
+          return layer.feature.properties.mag.toString();
+        });
+        markers.addLayer(geojsonLayer);
+        map.addLayer(markers);
       }
     },
   });
