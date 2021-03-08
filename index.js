2bb55285cc153a510308fd580bed4fd559e5b1ee
berkay <berkayoruc@hotmail.com>
2021-03-08 21:58:33 +0300

    faults

--- index.js
+++ index.js
@@ -29,53 +29,37 @@ $(document).ready(function () {
       });
     },
   });
-    // var geojson = new L.GeoJSON.AJAX("./assets/active_faults.geojson");
-    // geojson.on("data:loaded", function () {
-    //   geojson.addTo(mymap);
-    // });
-    $.ajax({
-      url: `./assets/active_faults.geojson`,
-      success: function (data) {
-        var vectorGrid = L.vectorGrid.slicer( data, {
+  $.ajax({
+    url: `./assets/active_faults.geojson`,
+    success: function (data) {
+      var vectorGrid = L.vectorGrid
+        .slicer(data, {
           rendererFactory: L.svg.tile,
-          // vectorTileLayerStyles: {
-          //   sliced: function(properties, zoom) {
-          //     var p = properties.mapcolor7 % 5;
-          //     return {
-          //       fillColor: p === 0 ? '#800026' :
-          //           p === 1 ? '#E31A1C' :
-          //           p === 2 ? '#FEB24C' :
-          //           p === 3 ? '#B2FE4C' : '#FFEDA0',
-          //       fillOpacity: 0.5,
-          //        //fillOpacity: 1,
-          //       stroke: true,
-          //       fill: true,
-          //       color: 'black',
-          //          //opacity: 0.2,
-          //       weight: 0,
-          //     }
-          //   }
-          // },
+          vectorTileLayerStyles: {
+            sliced: function(properties, zoom) {
+              // var p = properties.mapcolor7 % 5;
+              return {
+                // fillColor: p === 0 ? '#800026' :
+                //     p === 1 ? '#E31A1C' :
+                //     p === 2 ? '#FEB24C' :
+                //     p === 3 ? '#B2FE4C' : '#FFEDA0',
+                //  fillOpacity: 1,
+                stroke: true,
+                // fill: true,
+                color: 'red',
+                //  opacity: 0.2,
+                weight: 1,
+              }
+            }
+          },
           interactive: true,
-          getFeatureId: function(f) {
+          getFeatureId: function (f) {
             return f.properties.catalog_id;
-          }
+          },
         })
         .addTo(mymap);
-        // var tileOptions = {
-        //   maxZoom: 20, // max zoom to preserve detail on
-        //   tolerance: 5, // simplification tolerance (higher means simpler)
-        //   extent: 4096, // tile extent (both width and height)
-        //   buffer: 64, // tile buffer on each side
-        //   debug: 0, // logging level (0 to disable, 1 or 2)
-        //   indexMaxZoom: 0, // max zoom in the initial tile index
-        //   indexMaxPoints: 100000, // max number of points per tile in the index
-        // };
-        // var tileIndex = geojsonvt(data, tileOptions);
-        // var tile = tileIndex.getTile(z, x, y);
-        // var features = tile.features;
-        // console.log(features)
-      }})
+    },
+  });
   getEqs("all", "month", mymap, markers);
 });
 
